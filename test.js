c75fa1866ef326bcbfb38ecbcf64826adcbfe4ea
Mario Castro <mariocaster@gmail.com>
2015-09-10 08:34:14 +0200

    Update to use Singleton

--- test.js
+++ test.js
@@ -1,27 +1,21 @@
-import orientjs from 'orientjs';
+import OrientDbSingleton from './data/OrientDbSingleton.es6';
 
-let dbName = "jsTest";
-
-let server = orientjs({
-  host: 'localhost',
-  port: 2424,
-  username: 'root',
-  password: '1234'
-});
-
-let db = server.use(dbName);
+let db = OrientDbSingleton.getInstance();
 
 //Do some query
 db.record.get("#11:0").then(res => {
   console.log(res.name);
-});
-db.query("SELECT FROM Hobby WHERE title=:title", {params:{title:"cycling"}}).then(res => {
-  res.map(hobby => {
-    console.log(hobby.title);
-  })
-});
-db.select().from("User").all().then(res => {
-  res.map(user => {
-    console.log(user.name);
-  })
+
+  db.query("SELECT FROM Hobby WHERE title=:title", {params:{title:"cycling"}}).then(res => {
+    res.map(hobby => {
+      console.log(hobby.title);
+    });
+
+    db.select().from("User").all().then(res => {
+      res.map(user => {
+        console.log(user.name);
+      });
+      OrientDbSingleton.closeConnection();
+    });
+  });
 });
